2f458edf935fa6810c4df863265f33f49b4b2d81
Petter Sjöstrand <petter@Petters-iMac.local>
2023-05-21 23:50:37 +0200

    less verbose wget

--- Scripts/copyfromkewlbear.sh
+++ Scripts/copyfromkewlbear.sh
@@ -2,19 +2,19 @@
 mkdir Open3D
 cd Open3D
 
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/Assimp.xcframework.zip
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/Faiss.xcframework.zip
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/IrrXML.xcframework.zip
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/JPEG.xcframework.zip
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/jsoncpp.xcframework.zip
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/libOpen3D.a.xcframework.zip
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/libOpen3D_3rdparty_liblzf.a.xcframework.zip
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/libOpen3D_3rdparty_qhullcpp.a.xcframework.zip
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/libOpen3D_3rdparty_qhull_r.a.xcframework.zip
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/libOpen3D_3rdparty_rply.a.xcframework.zip
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/png.xcframework.zip
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/pybind.a.xcframework.zip
-wget https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/TBB.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/Assimp.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/Faiss.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/IrrXML.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/JPEG.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/jsoncpp.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/libOpen3D.a.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/libOpen3D_3rdparty_liblzf.a.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/libOpen3D_3rdparty_qhullcpp.a.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/libOpen3D_3rdparty_qhull_r.a.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/libOpen3D_3rdparty_rply.a.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/png.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/pybind.a.xcframework.zip
+wget -nv https://github.com/kewlbear/Open3D-iOS/releases/download/0.0.20220804014308/TBB.xcframework.zip
 
 
 TAG=0.0.`date +%Y%m%d%H%M%S`
@@ -23,7 +23,7 @@ sed s/TAG/$TAG/g ../Package.swift.in > Package.swift
 
 for f in *.zip
 do
-	rm Package.swift.in
+    echo "found $f"
 	mv Package.swift Package.swift.in
 	sed s/"$f"_CHECKSUM/`swift package compute-checksum $f`/ Package.swift.in > Package.swift
 done
@@ -33,3 +33,18 @@ mv Package.swift ..
 
 
 echo "::set-output name=tag::$TAG"
+
+
+
+
+# original
+for f in *.xcframework
+do
+    zip -r $f.zip $f
+    rm Package.swift.in
+    mv Package.swift Package.swift.in
+    sed s/"$f"_CHECKSUM/`swift package compute-checksum $f.zip`/ Package.swift.in > Package.swift
+done
+
+rm ../Package.swift
+mv Package.swift ..
