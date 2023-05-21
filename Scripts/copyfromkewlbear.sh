3eaa5b4ab7f02f9c300a668a76df81775a3dc36b
Petter Sjöstrand <petter@Petters-iMac.local>
2023-05-22 00:03:22 +0200

    removed forgotten snippet

--- Scripts/copyfromkewlbear.sh
+++ Scripts/copyfromkewlbear.sh
@@ -33,18 +33,3 @@ mv Package.swift ..
 
 
 echo "::set-output name=tag::$TAG"
-
-
-
-
-# original
-for f in *.xcframework
-do
-    zip -r $f.zip $f
-    rm Package.swift.in
-    mv Package.swift Package.swift.in
-    sed s/"$f"_CHECKSUM/`swift package compute-checksum $f.zip`/ Package.swift.in > Package.swift
-done
-
-rm ../Package.swift
-mv Package.swift ..
